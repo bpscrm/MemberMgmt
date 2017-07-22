6674bb6a3633340362ad43ba9e6af572c07a0479
bpscrm <liyayacat@hotmail.com>
2017-07-22 23:46:25 +0800

    removed the wrong ones

--- src/com/bp/wei/service/WeService.java
+++
@@ -1,9 +0,0 @@
-package com.bp.wei.service;
-
-import java.util.Map;
-
-public interface WeService {
-	public boolean valid(String signature, String timestamp, String nonce);
-	
-	public String process(Map<String, String> requestMap);
-}
