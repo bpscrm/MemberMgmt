68f8d68c16cbca8695418e07d1541d4309579830
oracli <17519386@qq.com>
2017-07-25 12:36:49 +0800

    u

--- src/com/bp/wei/controller/MemberMgmtController.java
+++ src/com/bp/wei/controller/MemberMgmtController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping
-public class MemberRegisterController {
-	public static Logger log = LoggerFactory.getLogger(MemberRegisterController.class);
+public class MemberMgmtController {
+	public static Logger log = LoggerFactory.getLogger(MemberMgmtController.class);
 	
 	@RequestMapping(value="register", method = RequestMethod.GET)
 	public String redirectRegister(){		
@@ -17,7 +17,8 @@ public class MemberRegisterController {
 	}
 	
 	@RequestMapping(value="msg_success", method = RequestMethod.GET)
-	public String redirectMsg_success(){		
+	public String redirectMsg_success(){	
+		System.out.println("吃吃吃");
 		return "msg_success";
 	}
 }
